2f80acfb8e6d96d1e4da9794ea7c30a72795d064
Samantha Wabi <swfullpotential@gmail.com>
2020-05-27 03:58:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++
@@ -1,8 +0,0 @@
-def peak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
-  elsif string == string.upcase
-    "NO, NOT SINCE 1938!"
-  else 
-    "HUH, SPEAK UP SONNY!"
-end
