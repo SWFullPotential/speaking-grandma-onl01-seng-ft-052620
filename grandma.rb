1783e03cec78c76edb782a07a1f8fd644ef4879d
Samantha Wabi <swfullpotential@gmail.com>
2020-05-27 04:13:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,8 +6,8 @@ def speak_to_grandma(string)
   elsif string == string.upcase
     "NO, NOT SINCE 1938!"
   elsif string == "Hi Nana, how are you?"
-    "HUH, SPEAK UP SONNY!"
+    "HUH?!, SPEAK UP SONNY!"
   else 
-    "HUH, SPEAK UP SONNY!"
+    "HUH?!, SPEAK UP SONNY!"
 end
 end
